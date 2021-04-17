2a22e9125ef34cbf4dc5d8c95fca8eae89119a05
Josue Hiram Alvarez Martinez <za18011110@zapopan.tecmm.edu.mx>
2021-04-16 21:44:32 -0500

    Perfil admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PagosComponent } from './pagos/pagos.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagosComponent  
+    PagosComponent,
+    AdminComponent  
   ],
   imports: [
     BrowserModule
