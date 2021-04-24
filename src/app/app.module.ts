c7b3b705ae7c061eefe4d4f07b3cc36bfb4af2e0
Josue Hiram Alvarez Martinez <za18011110@zapopan.tecmm.edu.mx>
2021-04-24 15:23:58 -0500

    probando mysql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PagosComponent } from './pagos/pagos.component';
+import { DbtestComponent } from './dbtest/dbtest.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagosComponent  
+    PagosComponent,
+    DbtestComponent  
   ],
   imports: [
     BrowserModule
---
+++ src/app/dbtest/dbtest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dbtest',
+  templateUrl: './dbtest.component.html',
+  styleUrls: ['./dbtest.component.css']
+})
+export class DbtestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
